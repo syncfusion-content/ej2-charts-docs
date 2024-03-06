ff9fe471651f2beba36b0eb9c89d7d0f53430759
Soundeswari-Selvam <soundeswari.selvam@syncfusion.com>
2024-03-06 11:23:29 +0530

    871723: Added circular chart 3d sample in TypeScript and JavaScript.

--- ej2-javascript/code-snippet/circular-chart/getting-started/index.js
+++ ej2-javascript/code-snippet/circular-chart/getting-started/index.js
@@ -16,7 +16,7 @@ var circularchart = new ej.charts.CircularChart3D({
                 name: 'x',
                 position: 'Outside',
                 font: {
-                    fontWeight: '600',
+                    fontWeight: '600'
                 },
                 connectorStyle: { length: '40px' }
             },
