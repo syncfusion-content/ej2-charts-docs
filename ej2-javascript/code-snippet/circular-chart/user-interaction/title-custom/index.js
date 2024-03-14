4342c8f3b446c0ccb428d5fb997f734cf2a58218
Soundeswari-Selvam <soundeswari.selvam@syncfusion.com>
2024-03-14 13:14:47 +0530

    871723: Added circular chart 3d features sample in TypeScript and JavaScript UG.

--- ej2-javascript/code-snippet/circular-chart/user-interaction/title-custom/index.js
+++ ej2-javascript/code-snippet/circular-chart/user-interaction/title-custom/index.js
@@ -2,13 +2,13 @@ var circularchart = new ej.charts.CircularChart3D({
     series: [
         {
             dataSource: [
-                { x: 'Saudi Arabia', y: 58, text: '58%' },
-                { x: 'Persian Gulf', y: 15, text: '15%' },
-                { x: 'Canada', y: 13, text: '13%' },
-                { x: 'Venezuela', y: 8, text: '8%' },
-                { x: 'Mexico', y: 3, text: '3%' },
-                { x: 'Russia', y: 2, text: '2%' },
-                { x: 'Miscellaneous', y: 1, text: '1%' }
+                { x: 'Saudi Arabia', y: 58 },
+                { x: 'Persian Gulf', y: 15 },
+                { x: 'Canada', y: 13 },
+                { x: 'Venezuela', y: 8 },
+                { x: 'Mexico', y: 3 },
+                { x: 'Russia', y: 2 },
+                { x: 'Miscellaneous', y: 1 }
             ],
             xName: 'x',
             yName: 'y'
